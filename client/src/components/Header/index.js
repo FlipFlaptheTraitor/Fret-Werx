7c02a7e0cfd566ba1e2f4ecd1fe1626df0bc3c3b
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-05-30 20:30:09 -0400

    added auth conditions to header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,38 +1,17 @@
-import React,{Component} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Auth from '../../utils/auth';
 import logo from '../../assets/images/fwLogo.png';
 
 
 
-class Header extends Component {
-  constructor(props){
-    super(props)
-  
-  this.state ={
-    isLoggedIn: true
-  }
-  }
-  render(){
-    if (this.state.isLoggedIn){
-      return(
-        <header className="d-flex justify-content-between position-relative logo-container">
-      <div className="logo-container">
-        <img src={logo} alt="Fret Werx logo on black background" />
-        <Link to="/">
-          <div className="logo-link"></div>
-        </Link>
-      </div>
-      <div className="nav-container">
-        <nav className="text-right">
-          <Link to="/">Home</Link>
-          <Link to="/forum">Forum</Link>
-          <Link to="/fret-builder">Fret Builder</Link>
-        </nav>
-      </div>
-    </header>
-      )
-    } else{
+const Header = () => {
+
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <header className="d-flex justify-content-between position-relative logo-container">
       <div className="logo-container">
@@ -43,17 +22,25 @@ class Header extends Component {
       </div>
       <div className="nav-container">
         <nav className="text-right">
-          <Link to="/">Home</Link>
+        <Link to="/">Home</Link>
           <Link to="/forum">Forum</Link>
           <Link to="/fret-builder">Fret Builder</Link>
-          <Link to="/login">Login</Link>
+        {Auth.loggedIn() ? (
+            <>
+             <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
           <Link to="/signup">Signup</Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
   );
-  };
-};
 };
 
 export default Header;
