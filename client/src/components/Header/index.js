1e18e249070fa43c6e549b59d0993d3eae1478a4
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-05-30 20:10:18 -0400

    else statement for logged in

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { Link } from 'react-router-dom';
 
 import logo from '../../assets/images/fwLogo.png';
 
 
-const Header = () => {
+
+class Header extends Component {
+  constructor(props){
+    super(props)
+  
+  this.state ={
+    isLoggedIn: true
+  }
+  }
+  render(){
+    if (this.state.isLoggedIn){
+      return(
+        <header className="d-flex justify-content-between position-relative logo-container">
+      <div className="logo-container">
+        <img src={logo} alt="Fret Werx logo on black background" />
+        <Link to="/">
+          <div className="logo-link"></div>
+        </Link>
+      </div>
+      <div className="nav-container">
+        <nav className="text-right">
+          <Link to="/">Home</Link>
+          <Link to="/forum">Forum</Link>
+          <Link to="/fret-builder">Fret Builder</Link>
+        </nav>
+      </div>
+    </header>
+      )
+    } else{
   return (
     <header className="d-flex justify-content-between position-relative logo-container">
       <div className="logo-container">
@@ -24,6 +52,8 @@ const Header = () => {
       </div>
     </header>
   );
+  };
+};
 };
 
 export default Header;
