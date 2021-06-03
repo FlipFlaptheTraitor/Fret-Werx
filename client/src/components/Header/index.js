104800edaed740cc842d61c5b134387d40a20884
ChillyWilly <noreply>
2021-06-03 01:59:18 -0400

    responsive nav

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -23,26 +23,31 @@ const Header = () => {
           <div className="logo-link"></div>
         </Link>
       </div>
-      <div className="nav-container">
-        <nav className="text-right">
-          <Link to="/">Home</Link>
-          <Link to="/forum">Forum</Link>
+      <nav className="nav-container nav navbar navbar-dark navbar-expand-lg">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <Link to="/" className="nav-item">Home</Link>
+          <Link to="/forum" className="nav-item">Forum</Link>
         {Auth.loggedIn() ? (
             <>
-              <Link to="/fret-builder">Fret Builder</Link>
-              <Link to={`/my-frets/${curUser}`}>My Frets</Link>
-              <a href="/" onClick={logout}>
+              <Link to="/fret-builder" className="nav-item">Fret Builder</Link>
+              <Link to={`/my-frets/${curUser}`} className="nav-item">My Frets</Link>
+              <a href="/" onClick={logout} className="nav-item">
                 Logout
               </a>
             </>
           ) : (
             <>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
+              <Link to="/login" className="nav-item">Login</Link>
+              <Link to="/signup" className="nav-item">Signup</Link>
             </>
           )}
-        </nav>
-      </div>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
